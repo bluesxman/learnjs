a24a4b049f44d7397d91a88be4e352067ab6bbc1
Jon Newton <jon.newton@gmail.com>
2017-08-13 10:48:21 -0600

    support nav to say Finished

--- public/app.js
+++ public/app.js
@@ -42,9 +42,22 @@ learnjs.problemView = function(data) {
     });
   }
 
+  learnjs.buildCorrectFlash = function(problemNum) {
+      var correctFlash = learnjs.template('correct-flash');
+      var link = correctFlash.find('a');
+      if (problemNum < learnjs.problems.length) {
+        link.attr('href', '#problem-' + (problemNum + 1));
+      } else {
+        link.attr('href', '');
+        link.text("You're Finished!");
+      }
+      return correctFlash;
+  }
+
   function checkAnswerClick() {
     if (checkAnswer()) {
-      learnjs.flashElement(resultFlash, 'Correct!');
+      var correctFlash = buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, correctFlash);
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
