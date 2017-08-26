f28521d8095373cb3e7afe52e814b6e41d2b5877
Jon Newton <jon.newton@gmail.com>
2017-08-26 08:10:46 -0600

    skip button

--- public/app.js
+++ public/app.js
@@ -64,6 +64,14 @@ learnjs.problemView = function(data) {
     return false;
   }
 
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
   learnjs.applyObject(problemData, view);
@@ -85,6 +93,7 @@ learnjs.showView = function(hash) {
   var viewFn = routes[hashParts[0]];
 
   if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
@@ -94,4 +103,8 @@ learnjs.appOnReady = function() {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
+}
+
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
 }
